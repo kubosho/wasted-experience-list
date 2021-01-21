18940a2340dd908d622d183f10967257a810c66f
Shota Kubota <ta2@o2p.jp>
2021-01-21 22:03:06 +0900

    feat(itemValue): add id property

--- src/itemValue.ts
+++ src/itemValue.ts
@@ -1,18 +1,21 @@
 import { milliseconds } from './time/millisecondsType';
 
 export interface ItemValue {
+    id: string;
     name: string;
     url: string;
     time: milliseconds;
 }
 
 interface Param {
+    id: string;
     name: string;
     url: string;
     time: milliseconds;
 }
 
-export const createItemValue = ({ name, url, time }: Param): ItemValue => ({
+export const createItemValue = ({ id, name, url, time }: Param): ItemValue => ({
+    id,
     name,
     url,
     time,
