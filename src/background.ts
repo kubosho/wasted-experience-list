e130dbf76e21beeddc5f3d1512d7898c1359d587
Shota Kubota <ta2@o2p.jp>
2021-02-05 01:18:53 +0900

    feat(src): implement background page

---
+++ src/background.ts
@@ -0,0 +1,9 @@
+import { createTimeOnPage } from './timeOnPage';
+
+function main(): void {
+    const timeOnPage = createTimeOnPage();
+
+    chrome.tabs.onActivated.addListener(async ({ tabId }) => timeOnPage.track(tabId));
+}
+
+main();
