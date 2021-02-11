60e62cb758305d7f885a9bcd56f21fe485938ddb
Shota Kubota <ta2@o2p.jp>
2021-02-11 14:09:08 +0900

    feat(totalTimeCalculator): return 0 for an empty array

--- src/time/totalTimeCalculator.ts
+++ src/time/totalTimeCalculator.ts
@@ -3,5 +3,9 @@ import { ItemValue } from '../wasted_experience_item/itemValue';
 import { milliseconds } from './millisecondsType';
 
 export function calcTotalTime(itemValueList: ItemValue[]): milliseconds {
+    if (itemValueList.length === 0) {
+        return 0;
+    }
+
     return itemValueList.map((item) => item.time).reduce((accumulator, current) => accumulator + current);
 }
