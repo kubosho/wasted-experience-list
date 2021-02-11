58c12e910886416e43e899b5b2a110b188a5767a
Shota Kubota <ta2@o2p.jp>
2021-02-12 00:02:59 +0900

    feat(ItemTable): apply spectrum-css based style

--- src/components/ItemTable.tsx
+++ src/components/ItemTable.tsx
@@ -21,41 +21,62 @@ export const ItemTable = ({ itemValueList, onBlurInputForm, onDeleteItem }: Item
     }, []);
 
     return (
-        <table>
-            {itemValueMapEntries.map(([key, value], i) => (
-                <tr key={`${key}_${i}`}>
-                    <td>
-                        <input type="hidden" id={ItemTableFormName.Id} name={ItemTableFormName.Id} value={key} />
-                        <label>
-                            Name:
+        <table className="spectrum-Table">
+            <tbody className="spectrum-Table-body">
+                {itemValueList.map((value, index) => (
+                    <tr className="spectrum-Table-row" key={`${value.id}_${index}`}>
+                        <td className="spectrum-Table-cell">
                             <input
-                                ref={itemNameRef}
-                                type="text"
-                                name={ItemTableFormName.Name}
-                                id={ItemTableFormName.Name}
-                                value={value.name}
-                                onBlur={(event) => onBlurInputForm(event, key)}
+                                type="hidden"
+                                id={ItemTableFormName.Id}
+                                name={ItemTableFormName.Id}
+                                value={value.id}
                             />
-                        </label>
-                    </td>
-                    <td>
-                        <label>
-                            URL:
-                            <input
-                                type="url"
-                                name={ItemTableFormName.Url}
-                                id={ItemTableFormName.Url}
-                                value={value.url}
-                                onBlur={(event) => onBlurInputForm(event, key)}
-                            />
-                        </label>
-                    </td>
-                    <td>{convertMsToTime(value.time)}</td>
-                    <td>
-                        <button onClick={() => onDeleteItem(key)}>-</button>
-                    </td>
-                </tr>
-            ))}
+                            <label>
+                                Name:
+                                <input
+                                    ref={itemNameRef}
+                                    type="text"
+                                    name={ItemTableFormName.Name}
+                                    id={ItemTableFormName.Name}
+                                    value={value.name}
+                                    onBlur={(event) => onBlurInputForm(event, index)}
+                                />
+                            </label>
+                        </td>
+                        <td className="spectrum-Table-cell">
+                            <label>
+                                URL:
+                                <input
+                                    type="url"
+                                    name={ItemTableFormName.Url}
+                                    id={ItemTableFormName.Url}
+                                    value={value.url}
+                                    onBlur={(event) => onBlurInputForm(event, index)}
+                                />
+                            </label>
+                        </td>
+                        <td className="spectrum-Table-cell">{convertMsToTime(value.time)}</td>
+                        <td className="spectrum-Table-cell">
+                            <button
+                                className="spectrum-Button spectrum-Button--warning spectrum-Button--sizeS"
+                                onClick={() => onDeleteItem(index)}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="spectrum-Icon spectrum-Icon--sizeS"
+                                    focusable="false"
+                                    aria-hidden="true"
+                                    aria-label="Delete"
+                                >
+                                    <path d="M15.75,3H12V2a1,1,0,0,0-1-1H6A1,1,0,0,0,5,2V3H1.25A.25.25,0,0,0,1,3.25v.5A.25.25,0,0,0,1.25,4h1L3.4565,16.55a.5.5,0,0,0,.5.45H13.046a.5.5,0,0,0,.5-.45L14.75,4h1A.25.25,0,0,0,16,3.75v-.5A.25.25,0,0,0,15.75,3ZM5.5325,14.5a.5.5,0,0,1-.53245-.46529L5,14.034l-.5355-8a.50112.50112,0,0,1,1-.067l.5355,8a.5.5,0,0,1-.46486.53283ZM9,14a.5.5,0,0,1-1,0V6A.5.5,0,0,1,9,6ZM11,3H6V2h5Zm1,11.034a.50112.50112,0,0,1-1-.067l.5355-8a.50112.50112,0,1,1,1,.067Z" />
+                                </svg>
+                                <span className="spectrum-Button-label">Delete</span>
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
         </table>
     );
 };
