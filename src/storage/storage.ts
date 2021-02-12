e2a7b41529b8f69e8aa213414f4b924da0f7bc5f
Shota Kubota <ta2@o2p.jp>
2021-02-12 12:39:40 +0900

    refactor(src): remove unused files

--- src/storage/storage.ts
+++
@@ -1,7 +0,0 @@
-import { DOMStorageLike } from './domStorageLike';
-
-export const STORAGE_KEY = 'wasted_experience_list';
-
-export function createStorageWrapper(): DOMStorageLike {
-    return window.localStorage;
-}
