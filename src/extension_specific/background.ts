6d5506dab09bc03dc0e88faee7000b28634d3928
Shota Kubota <ta2@o2p.jp>
2021-02-12 12:39:25 +0900

    feat(extension_specific): update background script

--- src/extension_specific/background.ts
+++ src/extension_specific/background.ts
@@ -1,10 +1,24 @@
-import { createTimeOnPage } from '../timeOnPage';
+import { TabChangeInfoStatus } from '../tab/tabChangeInfo';
+import { createTimeTrackerOfSpentOnPage } from '../time/timeTrackerOfSpentOnPage';
 
 function main(): void {
-    const timeOnPage = createTimeOnPage();
+    const timeTrackerOfSpentOnPage = createTimeTrackerOfSpentOnPage();
 
-    chrome.tabs.onActivated.addListener(async ({ tabId }) => await timeOnPage.track(tabId));
-    chrome.tabs.onUpdated.addListener(async (tabId) => await timeOnPage.track(tabId));
+    if (!timeTrackerOfSpentOnPage) {
+        return;
+    }
+
+    chrome.tabs.onActivated.addListener((activeInfo) => {
+        timeTrackerOfSpentOnPage.untrack();
+        timeTrackerOfSpentOnPage.track(activeInfo.tabId);
+    });
+
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+        if (changeInfo.status === TabChangeInfoStatus.Complete) {
+            timeTrackerOfSpentOnPage.untrack();
+            timeTrackerOfSpentOnPage.track(tabId);
+        }
+    });
 }
 
 main();
