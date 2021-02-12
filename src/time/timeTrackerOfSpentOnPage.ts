fe298cebd718f8fd4dfb72039dbf77ef6293888e
Shota Kubota <ta2@o2p.jp>
2021-02-12 18:29:29 +0900

    feat(timeTrackerOfSpendOnPage): get itemValueList when recording time spent view a page

--- src/time/timeTrackerOfSpentOnPage.ts
+++ src/time/timeTrackerOfSpentOnPage.ts
@@ -6,28 +6,26 @@ import { getSyncStorage } from '../storage/syncStorage';
 
 class TimeTrackerOfSpentOnPage {
     private _storage: StorageWrapper;
-    private _itemValueList: ItemValue[] | null;
     private _intervalId: number | null;
 
     constructor(storage: StorageWrapper) {
         this._storage = storage;
-        this._itemValueList = null;
         this._intervalId = null;
-
-        this._storage?.get<ItemValue[]>(STORAGE_KEY).then((valueList) => (this._itemValueList = valueList));
     }
 
     async track(tabId: number): Promise<void> {
         const tab = await getTabData(tabId);
         const pageUrl = tab.url;
 
-        const index = this._itemValueList?.findIndex((itemValue) => itemValue.url === pageUrl) ?? -1;
-        if (index < 0) {
-            return;
-        }
+        this._intervalId = window.setInterval(async () => {
+            const itemValueList = await this._storage?.get<ItemValue[]>(STORAGE_KEY);
+            const index = itemValueList.findIndex((itemValue) => itemValue.url === pageUrl);
 
-        this._intervalId = window.setInterval(() => {
-            this._saveToStorage(index);
+            if (index < 0) {
+                return;
+            }
+
+            this._saveToStorage(itemValueList, index);
         }, SECONDS);
     }
 
@@ -38,15 +36,11 @@ class TimeTrackerOfSpentOnPage {
         }
     }
 
-    private _saveToStorage(index: number): void {
-        if (!this._itemValueList) {
-            return;
-        }
+    private async _saveToStorage(itemValueList: ItemValue[], index: number): Promise<void> {
+        const value = itemValueList[index];
 
-        const value = this._itemValueList[index];
-
-        this._itemValueList[index] = createItemValue({ ...value, time: (value.time += SECONDS) });
-        this._storage.set(STORAGE_KEY, this._itemValueList);
+        itemValueList[index] = createItemValue({ ...value, time: (value.time += SECONDS) });
+        this._storage.set(STORAGE_KEY, itemValueList);
     }
 }
 
