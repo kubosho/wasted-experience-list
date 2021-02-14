d32c135e00de91c4e00dc4b375ca195d39849f0f
Shota Kubota <ta2@o2p.jp>
2021-02-14 19:03:34 +0900

    feat(src): reduce a frequency of access to storage

--- src/background.ts
+++ src/background.ts
@@ -3,7 +3,6 @@ import { createTimeTrackerOfSpentOnPage } from './time/timeTrackerOfSpentOnPage'
 
 function main(): void {
     const timeTrackerOfSpentOnPage = createTimeTrackerOfSpentOnPage();
-
     if (!timeTrackerOfSpentOnPage) {
         return;
     }
@@ -11,13 +10,24 @@ function main(): void {
     chrome.tabs.onActivated.addListener((activeInfo) => {
         timeTrackerOfSpentOnPage.untrack();
         timeTrackerOfSpentOnPage.track(activeInfo.tabId);
+
+        const itemValueList = timeTrackerOfSpentOnPage.itemValueList;
+        itemValueList && timeTrackerOfSpentOnPage.saveToStorage(itemValueList);
     });
 
     chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
         if (changeInfo.status === TabChangeInfoStatus.Complete) {
             timeTrackerOfSpentOnPage.untrack();
             timeTrackerOfSpentOnPage.track(tabId);
+
+            const itemValueList = timeTrackerOfSpentOnPage.itemValueList;
+            itemValueList && timeTrackerOfSpentOnPage.saveToStorage(itemValueList);
         }
+    });
+
+    chrome.tabs.onRemoved.addListener(() => {
+        const itemValueList = timeTrackerOfSpentOnPage.itemValueList;
+        itemValueList && timeTrackerOfSpentOnPage.saveToStorage(itemValueList);
     });
 }
 
--- src/time/timeTrackerOfSpentOnPage.ts
+++ src/time/timeTrackerOfSpentOnPage.ts
@@ -7,10 +7,20 @@ import { getSyncStorage } from '../storage/syncStorage';
 class TimeTrackerOfSpentOnPage {
     private _storage: StorageWrapper;
     private _intervalId: number | null;
+    private _itemValueList: ItemValue[] | null;
 
     constructor(storage: StorageWrapper) {
         this._storage = storage;
         this._intervalId = null;
+        this._itemValueList = null;
+
+        this._storage?.get<ItemValue[]>(STORAGE_KEY).then((valueList) => {
+            this._itemValueList = valueList;
+        });
+    }
+
+    get itemValueList(): ItemValue[] | null {
+        return this._itemValueList;
     }
 
     async track(tabId: number): Promise<void> {
@@ -18,17 +28,20 @@ class TimeTrackerOfSpentOnPage {
         const pageUrl = tab.url;
 
         this._intervalId = window.setInterval(async () => {
-            const itemValueList = await this._storage?.get<ItemValue[]>(STORAGE_KEY);
-            const index = itemValueList.findIndex((itemValue) => itemValue.url === pageUrl);
+            if (!this._itemValueList) {
+                return;
+            }
 
+            const index = this._itemValueList.findIndex((itemValue) => itemValue.url === pageUrl);
             if (index < 0) {
                 return;
             }
 
-            const value = itemValueList[index];
-            itemValueList[index] = createItemValue({ ...value, time: (value.time += SECONDS) });
+            const prevValue = this._itemValueList[index];
+            const newValue = createItemValue({ ...prevValue, time: (prevValue.time += SECONDS) });
+            const newList = replaceValueAtItemValueList(this._itemValueList, index, newValue);
 
-            this._saveToStorage(itemValueList);
+            this._itemValueList = newList;
         }, SECONDS);
     }
 
@@ -39,11 +52,15 @@ class TimeTrackerOfSpentOnPage {
         }
     }
 
-    private _saveToStorage(itemValueList: ItemValue[]): void {
-        setTimeout(() => {
-            this._storage.set(STORAGE_KEY, itemValueList);
-        });
+    saveToStorage(itemValueList: ItemValue[]): void {
+        this._storage.set(STORAGE_KEY, itemValueList);
     }
+}
+
+function replaceValueAtItemValueList(baseList: ItemValue[], index: number, value: ItemValue): ItemValue[] {
+    const newList = [...baseList];
+    newList[index] = value;
+    return newList;
 }
 
 export function createTimeTrackerOfSpentOnPage(): TimeTrackerOfSpentOnPage | null {
