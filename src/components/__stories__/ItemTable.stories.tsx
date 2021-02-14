39ea65466c8d8f4efd4af52b6303c2de2a4f06df
Shota Kubota <ta2@o2p.jp>
2021-02-15 00:48:27 +0900

    fix(ItemTable): fix incorrect props

--- src/components/__stories__/ItemTable.stories.tsx
+++ src/components/__stories__/ItemTable.stories.tsx
@@ -13,14 +13,14 @@ export default {
 
 const Template = (props: ItemTableProps): JSX.Element => <ItemTable {...props} />;
 
-const itemValueMap = new Map();
-
-itemValueMap.set('hash1', { name: 'Twitter', url: 'https://twitter.com/', time: 60 * 60 * 4 * 1000 });
-itemValueMap.set('hash2', { name: 'Facebook', url: 'https://www.facebook.com/', time: 60 * 60 * 3 * 1000 });
-itemValueMap.set('hash3', { name: 'Hatena', url: 'https://b.hatena.ne.jp/', time: 60 * 60 * 2 * 1000 });
+const itemValueList = [
+    { id: 'hash1', name: 'Twitter', url: 'https://twitter.com/', time: 60 * 60 * 4 * 1000 },
+    { id: 'hash2', name: 'Facebook', url: 'https://www.facebook.com/', time: 60 * 60 * 3 * 1000 },
+    { id: 'hash3', name: 'Hatena', url: 'https://b.hatena.ne.jp/', time: 60 * 60 * 2 * 1000 },
+];
 
 export const ItemTableComponent = Template.bind(this, {
-    itemValueMap,
+    itemValueList,
     onBlurInputForm: () => {
         return;
     },
