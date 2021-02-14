203cdd5cdec9a3d0a37e567d7e974d3c6f29d8bb
Shota Kubota <ta2@o2p.jp>
2021-02-14 20:51:48 +0900

    feat(background): implement Background class

--- src/background.ts
+++ src/background.ts
@@ -1,42 +1,49 @@
 import { getTabData } from './tab/tabData';
 import { TabChangeInfoStatus } from './tab/tabChangeInfo';
-import { createTimeTrackerOfSpentOnPage } from './time/timeTrackerOfSpentOnPage';
+import { TimeTrackerOfSpentOnPage, createTimeTrackerOfSpentOnPage } from './time/timeTrackerOfSpentOnPage';
 
-const timeTrackerOfSpentOnPage = createTimeTrackerOfSpentOnPage();
+class Background {
+    private _timeTrackerOfSpentOnPage: TimeTrackerOfSpentOnPage | null;
 
-function main(): void {
-    chrome.tabs.onActivated.addListener(({ tabId }) => {
-        activateTrack(tabId);
-        saveStorage();
-    });
-
-    chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-        if (changeInfo.status === TabChangeInfoStatus.Complete) {
-            activateTrack(tabId);
-            saveStorage();
-        }
-    });
-
-    chrome.tabs.onRemoved.addListener(() => {
-        saveStorage();
-    });
-}
-
-main();
-
-async function activateTrack(tabId: number): Promise<void> {
-    const tab = await getTabData(tabId);
-    const pageUrl = tab.url;
-
-    if (!pageUrl) {
-        return;
+    constructor() {
+        this._timeTrackerOfSpentOnPage = createTimeTrackerOfSpentOnPage();
+        this._initEventListener();
     }
 
-    timeTrackerOfSpentOnPage?.untrack();
-    timeTrackerOfSpentOnPage?.track(pageUrl);
+    private async _activateTrack(tabId: number): Promise<void> {
+        const tab = await getTabData(tabId);
+        const pageUrl = tab.url;
+
+        if (!pageUrl) {
+            return;
+        }
+
+        this._timeTrackerOfSpentOnPage?.untrack();
+        this._timeTrackerOfSpentOnPage?.track(pageUrl);
+    }
+
+    private _saveStorage(): void {
+        const itemValueList = this._timeTrackerOfSpentOnPage?.itemValueList;
+        itemValueList && this._timeTrackerOfSpentOnPage?.saveToStorage(itemValueList);
+    }
+
+    private _initEventListener(): void {
+        chrome.tabs.onActivated.addListener(({ tabId }) => {
+            this._activateTrack(tabId);
+            this._saveStorage();
+        });
+
+        chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+            if (changeInfo.status === TabChangeInfoStatus.Complete) {
+                this._activateTrack(tabId);
+                this._saveStorage();
+            }
+        });
+
+        chrome.tabs.onRemoved.addListener(() => {
+            this._saveStorage();
+        });
+    }
 }
 
-function saveStorage(): void {
-    const itemValueList = timeTrackerOfSpentOnPage?.itemValueList;
-    itemValueList && timeTrackerOfSpentOnPage?.saveToStorage(itemValueList);
-}
+new Background();
