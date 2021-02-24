e3a459069ccaedf9ac76607311e6bd73a79b9896
Shota Kubota <ta2@o2p.jp>
2021-02-25 01:17:53 +0900

    feat(itemValueListConnectPort): add function to connect to port

--- src/wasted_experience_item/itemValueListConnectPort.ts
+++ src/wasted_experience_item/itemValueListConnectPort.ts
@@ -1,1 +1,5 @@
 export const itemValueListConnectPort = { name: 'itemValueListConnectPort' };
+
+export const connectItemValueListConnectPort = (): chrome.runtime.Port => {
+    return chrome.runtime.connect(itemValueListConnectPort);
+};
