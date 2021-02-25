c66be3443d0e6022843a7f730b96e9882e2c4235
Shota Kubota <ta2@o2p.jp>
2021-02-26 00:34:05 +0900

    feat(badge): implement set badge text function

---
+++ src/badge/badgeText.ts
@@ -0,0 +1,17 @@
+import { convertMsToMMSSFormat, convertMsToTime } from '../time/millisecondsToTimeConverter';
+import { HOUR_TO_MILLISECONDS } from '../time/time';
+
+export function setBadgeText(time?: number): void {
+    if (!time) {
+        chrome.browserAction.setBadgeText({
+            text: '',
+        });
+        return;
+    }
+
+    const badgeText = time < 1 * HOUR_TO_MILLISECONDS ? convertMsToMMSSFormat(time) : convertMsToTime(time);
+
+    chrome.browserAction.setBadgeText({
+        text: badgeText,
+    });
+}
