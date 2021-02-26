b0ce7ad33e180fb208055c5e9d2ec37d463dfcd9
Shota Kubota <ta2@o2p.jp>
2021-02-26 22:38:38 +0900

    feat(popupInitialStateConnectPort): add function to connect to port

--- src/popupInitialStateConnectPort.ts
+++ src/popupInitialStateConnectPort.ts
@@ -1,1 +1,5 @@
 export const popupInitialStateConnectPort = { name: 'popupInitialStateConnectPort' };
+
+export const connectPopupInitialStateConnectPort = (): chrome.runtime.Port => {
+    return chrome.runtime.connect(popupInitialStateConnectPort);
+};
