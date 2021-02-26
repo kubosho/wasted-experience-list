ebd64df11441a96bb210bc5d485e964a393f8bcd
Shota Kubota <ta2@o2p.jp>
2021-02-26 23:14:14 +0900

    feat(timeTrackerOfSpentOnPage): update interface

--- src/time/timeTrackerOfSpentOnPage.ts
+++ src/time/timeTrackerOfSpentOnPage.ts
@@ -2,62 +2,58 @@ import { SECONDS } from '../time/time';
 import { createItemValue, ItemValue } from '../wasted_experience_item/itemValue';
 
 export interface TimeTrackerOfSpentOnPage {
-    track(
-        pageUrl: string,
-        itemValueList: ItemValue[],
-        callback?: (value: ItemValue | null, itemValueList: ItemValue[] | null) => void,
-    ): void;
-    startAutoTrack(
-        pageUrl: string,
-        itemValueList: ItemValue[],
-        callback?: (value: ItemValue | null, itemValueList: ItemValue[] | null) => void,
-    ): void;
+    setItemValueList(itemValueList: ItemValue[]): void;
+    track(pageUrl: string | null, callback?: (value: ItemValue, itemValueList: ItemValue[]) => void): void;
+    startAutoTrack(pageUrl: string | null, callback?: (value: ItemValue, itemValueList: ItemValue[]) => void): void;
     stopAutoTrack(callback?: () => void): void;
 }
 
 class TimeTrackerOfSpentOnPageImpl implements TimeTrackerOfSpentOnPage {
     private _intervalId: number | null;
+    private _itemValueList: ItemValue[] | null;
 
     constructor() {
         this._intervalId = null;
+        this._itemValueList = null;
     }
 
-    track(
-        pageUrl: string,
-        itemValueList: ItemValue[],
-        callback?: (value: ItemValue | null, itemValueList: ItemValue[] | null) => void,
-    ): void {
-        if (pageUrl === '' || itemValueList.length === 0) {
+    setItemValueList(itemValueList: ItemValue[]): void {
+        this._itemValueList = itemValueList;
+    }
+
+    track(pageUrl: string | null, callback?: (value: ItemValue, itemValueList: ItemValue[]) => void): void {
+        if (!pageUrl || pageUrl === '' || !this._itemValueList || this._itemValueList.length === 0) {
             return;
         }
 
-        const index = itemValueList.findIndex((itemValue) => itemValue.url === pageUrl);
+        const index = this._itemValueList?.findIndex((itemValue) => itemValue.url === pageUrl);
         if (index < 0) {
             return;
         }
 
-        const prevItemValue = itemValueList[index];
+        const prevItemValue = this._itemValueList[index];
         const itemValue = createItemValue({ ...prevItemValue, time: (prevItemValue.time += SECONDS) });
-        const newItemValueList = replaceValueAtItemValueList(itemValueList, index, itemValue);
+        const newItemValueList = replaceValueAtItemValueList(this._itemValueList, index, itemValue);
 
         callback && callback(itemValue, newItemValueList);
     }
 
-    startAutoTrack(
-        pageUrl: string,
-        itemValueList: ItemValue[],
-        callback?: (value: ItemValue | null, itemValueList: ItemValue[] | null) => void,
-    ): void {
-        if (pageUrl === '') {
+    startAutoTrack(pageUrl: string | null, callback?: (value: ItemValue, itemValueList: ItemValue[]) => void): void {
+        if (!pageUrl || pageUrl === '') {
             return;
         }
 
-        this._intervalId = window.setInterval(() => this.track(pageUrl, itemValueList, callback), SECONDS);
+        const startAutoTrack = (): void => {
+            this.track(pageUrl, callback);
+            this._intervalId = window.setTimeout(startAutoTrack, SECONDS);
+        };
+
+        startAutoTrack();
     }
 
     stopAutoTrack(callback?: () => void): void {
         if (this._intervalId !== null) {
-            clearInterval(this._intervalId);
+            clearTimeout(this._intervalId);
             this._intervalId = null;
             callback && callback();
         }
@@ -70,6 +66,6 @@ function replaceValueAtItemValueList(baseList: ItemValue[], index: number, value
     return newList;
 }
 
-export function createTimeTrackerOfSpentOnPage(): TimeTrackerOfSpentOnPage | null {
+export function createTimeTrackerOfSpentOnPage(): TimeTrackerOfSpentOnPage {
     return new TimeTrackerOfSpentOnPageImpl();
 }
