4b70d3007b5ef13dbbe40b0655aceb11d3d70441
Shota Kubota <ta2@o2p.jp>
2021-02-27 18:01:49 +0900

    chore(tailwind): add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: {
+        enabled: true,
+        content: ['./public/*.html', './public/**/*.html', './lib/*.js', './lib/**/*.js'],
+    },
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
