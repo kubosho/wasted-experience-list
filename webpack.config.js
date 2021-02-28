81a795936028067a8c083cc3733c4ee8077c6c0d
Shota Kubota <ta2@o2p.jp>
2021-02-28 18:39:18 +0900

    build(webpack): add split chunks config

--- webpack.config.js
+++ webpack.config.js
@@ -50,4 +50,26 @@ module.exports = {
     },
     mode: process.env.NODE_ENV,
     devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                // Disable default and vendors config.
+                // https://github.com/webpack/webpack/blob/6278ac6/lib/config/defaults.js#L898-L909
+                default: false,
+                defaultVendors: false,
+                framework: {
+                    name: 'framework',
+                    chunks: 'all',
+                    // Prevent framework chunks from being absorbed by other chunks.
+                    // https://github.com/vercel/next.js/pull/9250
+                    enforce: true,
+                    priority: 20,
+                    test: /[\\/]node_modules[\\/](preact|recoil)[\\/]/,
+                },
+            },
+            maxInitialRequests: 25,
+            minSize: 20000,
+        },
+    },
 };
